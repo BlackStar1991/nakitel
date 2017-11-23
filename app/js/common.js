cf2fa30f11f5bb901c565c7e1ba709a95a519ed9
BlackStar1991 <zirkaandry@ukr.net>
2017-11-24 00:13:57 +0200

    fulll work

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+window.onload = function () {
 
     var blackWrapper = $(".blackWrapper"),
         body = $("body");
@@ -10,49 +10,68 @@ window.onload = function() {
         arreaNavigation.toggleClass("hidden");
     });
 
+/// Mobile search
 
+    $(".js-search").on("click", function () {
+        $(".bl_search").toggleClass("bl_search__active");
+
+    });
+///// CEO text
+    $(".bl_ceo__showMore").on("click", function () {
+        $(".bl_ceo__wrapper").css({"overflow": "visible", "height": "auto"});
+        $(this).remove();
+
+    });
 
 //    Discount Products  - Скидки
 
-       $(".product_slider__full").owlCarousel({
-           items: 5,
-           loop: true,
-           nav: false,
-           navText: true,
-           dots: true,
-           center:true,
-           // dotsEach:true,
-           autoplay: false,
-           stopOnHover: true,
-           smartSpeed: 1000,
-           autoplayTimeout: 4000,
-           pagination: false,
-           responsiveClass: true,
-           responsive: {
-               1200: {
-                   items: 5
+    $(".product_slider__full").owlCarousel({
+        items: 5,
+        loop: true,
+        nav: false,
+        navText: true,
+        dots: true,
+        margin: 120,
+        dotsEach: false,
+        autoplay: true,
+        stopOnHover: true,
+        smartSpeed: 1000,
+        autoplayTimeout: 4000,
+        pagination: false,
+        responsiveClass: true,
+        responsive: {
+            1200: {
+                items: 5
 
-               },
-               991: {
-                   items: 3
+            },
+            991: {
+                margin: 20,
+                items: 3
 
-               },
-               550: {
-                   items: 2
+            },
+            550: {
+                autoplayTimeout: 2000,
+                dots: false,
+                items: 2
 
-               },
-               450: {
-                   items: 1
 
-               },
+            },
+            450: {
+                autoplayTimeout: 2000,
+                dots: false,
+                items: 1
 
-               320: {
-                   items: 1
+            },
 
-               }
+            320: {
+                autoplayTimeout: 2000,
+                dots: false,
+                items: 1
 
-           }
-       });
+            }
+
+        }
+    });
 
 
 };
